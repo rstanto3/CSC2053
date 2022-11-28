490feb4c5cad91d3cb99cf68d93b3b506efbdcf2
rstanto3 <rstanto3@villanova.edu>
2022-11-28 16:04:55 -0500

    added a python file

---
+++ example.py
@@ -0,0 +1,11 @@
+np = [1, 2, 3, 4, 5]
+i = 1
+while i < 5:
+    np[i] *= np[i-1]
+    i += 1
+print("using while: " + str(np))
+
+np = [1,2,3,4,5]
+for x in range(1,5):
+    np[x] *= np[x-1]
+print("using for: " + str(np))
